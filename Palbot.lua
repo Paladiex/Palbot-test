adcfc7eb4452f9f978811af94a859c083d9b2f16
Rodrigo Leote <rodrigo.leote@poatek.com>
2018-04-02 04:48:11 -0300

    Reset monX to 0 when picking storage

--- Palbot.lua
+++ Palbot.lua
@@ -1613,7 +1613,8 @@ function StorageFodderEvaluater()
       if fodderFill > 0 then
         click(Location (1511 - monX*156, 830 - monY*156))
         fodderFill = fodderFill - 1
-        monX = monX - 1 
+        monX = 0 
+        monY = 0
         if fodderFill < 1 then
           fodderStorageOkRegion:click(Pattern("ok.png"):similar(.80))
           if fodderInfoRegion:exists(Pattern("userLevel.png"):similar(.75), 0.1) then
